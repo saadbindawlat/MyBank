100c7dd1536891f87bc88eec0c8d4c1c5ec6c55d
saadbindawlat <sbdawlat@mun.ca>
2025-05-09 11:56:09 -0230

    Implement auth, appwrite, and sentry

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -60,35 +60,35 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
   try {
     const { account, database } = await createAdminClient();
 
-    newUserAccount = await account.create(
-      ID.unique(), 
-      email, 
-      password, 
-      `${firstName} ${lastName}`
-    );
+    // newUserAccount = await account.create(
+    //   ID.unique(), 
+    //   email, 
+    //   password, 
+    //   `${firstName} ${lastName}`
+    // );
 
-    if(!newUserAccount) throw new Error('Error creating user')
+    // if(!newUserAccount) throw new Error('Error creating user')
 
-    const dwollaCustomerUrl = await createDwollaCustomer({
-      ...userData,
-      type: 'personal'
-    })
+    // const dwollaCustomerUrl = await createDwollaCustomer({
+    //   ...userData,
+    //   type: 'personal'
+    // })
 
-    if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
+    // if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
 
-    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+    // const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
 
-    const newUser = await database.createDocument(
-      DATABASE_ID!,
-      USER_COLLECTION_ID!,
-      ID.unique(),
-      {
-        ...userData,
-        userId: newUserAccount.$id,
-        dwollaCustomerId,
-        dwollaCustomerUrl
-      }
-    )
+    // const newUser = await database.createDocument(
+    //   DATABASE_ID!,
+    //   USER_COLLECTION_ID!,
+    //   ID.unique(),
+    //   {
+    //     ...userData,
+    //     userId: newUserAccount.$id,
+    //     dwollaCustomerId,
+    //     dwollaCustomerUrl
+    //   }
+    // )
 
     const session = await account.createEmailPasswordSession(email, password);
 
